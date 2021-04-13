2e449a5c04ac581a780f4947697fdbe8e278b4cf
Stella Jin <stellajin@Stellas-MacBook-Air.local>
2021-04-13 23:28:59 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// eslint-disable-next-line
+import { Products, Navbar, Cart } from "./components";
+import { commerce } from "./lib/commerce";
 
-function App() {
+const App = () => {
+  // eslint-disable-next-line
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    //await the speciafic APIcall to the instance commerce, and destruct the data from the response which will be the products
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  //Accept two parameters and add to the cart, which are  product id and quantity
+  // eslint-disable-next-line
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart); // This is the cart after the items have been added
+  };
+
+  //useEffect hook to fetch the products immediately when the application loads
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []); //dependency array set to empty, only runs at the start on the render
+
+  console.log(cart);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar totalItems={cart.total_items} />
+      {/* <Products products={products} addToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton, // eslint-disable-next-line
+  Menu, // eslint-disable-next-line
+  MenuItem,
+  Typography,
+  Badge,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/logo.png";
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" color="inherit" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            <img
+              src={logo}
+              alt="Merca Prime"
+              height="25px"
+              className={classes.image}
+            />
+            Mercar Prime
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart Item" color="inherit">
+              <Badge color="secondary" badgeContent={totalItems}>
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
